2447b5ba271744aa70873fa9be167a00758caf96
annusinha1 <annusinha15029@gmail.com>
2023-02-01 17:45:13 +0530

    add this code

--- annu.php
+++ annu.php
@@ -1,17 +1,17 @@
 <?php
 if(isset($_POST["submit"]))
 {
-    $val = $_POST["Name"];
+    $val = intval($_POST["Name"]);
+    $clas = intval($_POST["Class"]);
     
     for ($i=1; $i<=10 ; $i++) { 
-        echo intval($val)*$i."<br>" ;
-        sdjdgshfjgfsd
-        dbdfkjghfdgkj
+        echo $clas*$i."<br>";
     }
 // echo $_POST["Name"];
 }
 ?>
  <form id="myform" role="form" action="" method="post">
         <input type="text" class="form-control" name="Name">  
+        <input type="text" class="form-control" name="Class">  
         <input type="submit"  name="submit">
     </form> 
